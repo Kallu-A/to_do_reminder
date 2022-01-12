4fb21f8373571b4ad35ea45d38740ee1fa396462
Kallu-A <lucas.aries@protonmail.com>
2022-01-12 10:42:36 +0100

    upgrade: front-end

--- server/static/js/script.js
+++ server/static/js/script.js
@@ -9,3 +9,26 @@ function hide_after_time() {
 function confirm_pop_up() {
     confirm("this is a confirm");
 }
+
+function set_upload() {
+    const fileName = document.querySelector(".file-name");
+    const uploadBtn = document.querySelector("#upload_button");
+    const preview = document.querySelector("preview");
+
+    uploadBtn.addEventListener("change", function(){
+        const file = this.files[0];
+        if(file){
+            const reader = new FileReader();
+            reader.onload = function(){
+                const result = reader.result;
+                preview.src = result;
+            }
+            cancelBtn.addEventListener("click", function(){
+                img.src = "";
+            })
+            reader.readAsDataURL(file);
+        }
+    });
+}
+
+
